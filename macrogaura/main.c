d667cab49083a5fa385d00831ba3b5d00d5d92d6
Le Bao Hiep <baohiep2013@gmail.com>
2019-07-20 23:15:25 +0700

    Initial Commit

---
+++ macrogaura/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  macrogaura
+//
+//  Created by Le Bao Hiep on 7/20/19.
+//  Copyright © 2019 Le Bao Hiep. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
